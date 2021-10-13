959b1e6d13d290464ee63c181207872955fce2fc
Stanislav <sundeev91@ya.ru>
2021-10-13 11:14:54 +0600

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## Description:
 This repository is built for Uber responsive layout.
 Layout taken from [Figma](https://www.figma.com/file/ivgb9OfADjPvRowi32CSgY/UBER_course)<br/>
-Site is available on github pages -> [WebSite](https://sens3ii.github.io/uber-responsive-layout/)<br/>
+Site is available on github pages -> [WebSite](https://sunstanislav.github.io/uber_page/)<br/>
 ## Technologies used:
 * HTML, SASS
 * Bootstrap GRID
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,73 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass')(require('sass'));
-const rename = require("gulp-rename");
-const autoprefixer = require('gulp-autoprefixer');
+const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
-// Static server
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const image = require('gulp-image');
+const htmlmin = require('gulp-htmlmin');
+// const deploy = require("gulp-gh-pages");
+const ghPages = require('gulp-gh-pages');
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
+
 gulp.task('server', function() {
-    browserSync.init({
+
+    browserSync({
         server: {
-            baseDir: "./"
+            baseDir: "dist"
         }
     });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
+
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min",
-          }))
-        .pipe(autoprefixer({
-			cascade: false
-		}))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
+
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"))
-    gulp.watch("scr/*.html").on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("*.html").on('change', gulp.parallel('html'));
 });
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+
+gulp.task('html', function () {
+    return gulp.src("*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
+
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('logo', function () {
+    return gulp.src("src/logo/**/**/*")
+        .pipe(gulp.dest("dist/logo"));
+});
+
+gulp.task('image', function () {
+    return gulp.src("src/img/**/**/*")
+        .pipe(image())
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'icons', 'html', 'image', 'logo'));
